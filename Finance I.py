413b6efe8695f143b9fad7a5b3f0a5bc9758d423
itsAsadbek <89941160+itsAsadbek@users.noreply.github.com>
2021-10-20 11:12:13 +0200

    Update Finance I.py

--- Finance I.py
+++ Finance I.py
@@ -94,3 +94,80 @@ def perpetuity(pp, r):
 
 
 print(perpetuity(500, 8))
+
+
+
+
+
+
+
+################################################################################# Class I
+def fv_simple(pv, r, n):
+    fv = pv*(1 + (r/100)*n)
+    return round(fv,2)
+
+print("The answer is",fv_simple(1000,5,5))
+
+
+def pv_simple(fv, r, n):
+    pv = fv/(1 + (r/100)*n)
+    return round(pv,2)
+
+print("The answer is",pv_simple(1250.0,5,5))
+
+
+def fv_annual(pv, r, n):
+    factor = (1 + r/100)**n
+    fv = pv*factor
+    return round(fv, 2)
+
+
+print("The answer is",fv_annual(12000, 3.75, 3))
+
+
+def pv_annual(fv, r, n):
+    factor = (1 + r/100)**n
+    pv = fv/factor
+    return round(pv, 2)
+
+
+print("The answer is",pv_annual(500, 20, 10))
+
+
+
+def fv_nonannual(pv, r, n, m):
+    factor = (1 + r/(100*m))**(m*n)
+    fv = pv*factor
+    return round(fv, 2)
+
+
+print("The answer is",fv_nonannual(12000, 3.75, 3, 4))
+
+
+
+def pv_nonannual(fv, r, n, m):
+    factor = (1 + r/(100*m))**(m*n)
+    pv = fv/factor
+    return round(pv, 2)
+
+
+print("The answer is",pv_nonannual(180.61, 12, 5, 4))
+
+
+
+def fv_continuous_compounding(pv, r, n):
+    rate = (r/100)*n
+    return round(pv*math.exp(rate),2)
+
+
+
+print("The answer is",fv_continuous_compounding(12000,3.75,3))
+
+
+def pv_continuous_compounding(fv, r, n):
+    rate = (r/100)*n
+    return round(fv/math.exp(rate),2)
+
+
+
+print("The answer is",pv_continuous_compounding(13428.87,3.75,3))
